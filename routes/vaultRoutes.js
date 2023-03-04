28b12da79cd8b98430a557d95c9b2bdfd072ea9c
GLVSKiriti <glvskiriti2003369@gmail.com>
2023-03-04 18:25:52 +0530

    Backend for login and signup is done; Backend for getAllVaults and addVault is also done;

---
+++ routes/vaultRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllVaults,
+  addVault,
+  displayVault,
+} = require("../controllers/vaults");
+
+const { verifyToken } = require("../middlewares/authMiddleware");
+
+router.get("/getAllVaults", verifyToken, getAllVaults);
+router.post("/addVault", verifyToken, addVault);
+router.get("/displayVault", verifyToken, displayVault);
+
+module.exports = router;
